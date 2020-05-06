0ddf009ed9583a42f1ae01ee3300d269c5397e74
SoninVI <v.sonin1986@gmail.com>
2020-05-06 18:36:29 +0300

    Сделан первый экран

---
+++ js/js.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
+var instance = M.Carousel.init({
+  fullWidth: true,
+  indicators: true
+});
+
+// Or with jQuery
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true,
+  duration: 300,
+
+});
